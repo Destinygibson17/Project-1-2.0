7d5f39628a4d87712eea02d5cde6f88975d12aec
xxAngelfallxx <44912178+xxAngelfallxx@users.noreply.github.com>
2018-12-12 13:38:04 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,8 @@
-/******************************************
-Treehouse Techdegree:
-FSJS project 1 - A Random Quote Generator
-******************************************/
 
-// Study guide for this project - https://drive.google.com/file/d/1s5grutGuQFwJcQP8bFwEI69Q8FCkGdDk/view?usp=sharing
 
 
 /***
-  Create the array of quote objects and name it `quotes`.
-  Add at least five quote objects to the `quotes` array.
-  Give each quote object a `quote` and `source` property.
-
-  Recommended:
-    - Add at least one `year` and/or `citation` property to at least one
-      quote object.
+This is my object array. It contains my favorite quotes about reading.
 ***/
 
 var quotes = [
@@ -60,27 +49,18 @@ var quotes = [
 ];
 
 
-/***
-  Create the `getRandomQuote` function to:
-   - generate a random number
-   - use the random number to `return` a random quote object from the
-     `quotes` array.
-***/
+/*
+This function will generate a random number and pick the quote that matches that number within the object array.
+*/
 
 function getRandomQuote() {
 var pickQuote = Math.floor(Math.random()* quotes.length);
 return (quotes[pickQuote]);
 
 }
-/***
-  Create the `printQuote` function to:
-   - call the `getRandomQuote` function and assign it to a variable.
-   - use the properties of the quote object stored in the variable to
-     create your HTML string.
-   - use conditionals to make sure the optional properties exist before
-     they are added to the HTML string.
-   - set the `innerHTML` of the `quote-box` div to the HTML string.
-***/
+/*
+This is my printQuote. It takes the information and prints it to the page.
+*/
 
 
 function printQuote() {
@@ -97,14 +77,8 @@ console.log(data);
 }
 
 
-/***
-  When the "Show another quote" button is clicked, the event listener
-  below will be triggered, and it will call, or "invoke", the `printQuote`
-  function. So do not make any changes to the line of code below this
-  comment.
-***/
+/*
+When you press the button, you get a new quote. Yay!
+*/
 
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
